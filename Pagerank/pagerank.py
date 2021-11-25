07be61a374eb9292641b29674d8b18027cc842fb
Ruben Bromée <tharubb1992@gmail.com>
2021-11-25 11:18:17 +0100

    pagerank implementation

--- Pagerank/pagerank.py
+++ Pagerank/pagerank.py
@@ -25,9 +25,14 @@ while a:
         edge_list[i][1] = t[2]
         i = i + 1
 
+
 # Creating link matrix, binary
 for i in range(len(edge_list)):
-    M[edge_list[i][1]][edge_list[i][0]] = 1 
+    M[edge_list[i][1]][edge_list[i][0]] = 1
+
+row_sum = M.sum(axis=1)
+m1 = max(row_sum)
+print(np.where(row_sum == m1))
 
 # Scaling with number of outlinks for each column
 column_sum = M.sum(axis=0)
@@ -58,18 +63,46 @@ A = alpha * P + (1 - alpha) * 1/len(column_sum) * eeT
 
 
 
-epsilon = 10e-4
+epsilon = 10e-5
 residual = 1
 i = 0
 
-while residual > epsilon and i < 100:
-    z = np.ones(len(column_sum)) / len(column_sum)
-    v = np.ones(len(column_sum)) / len(column_sum)
-    yhat = alpha * M * z
-    beta = 1 - np.linalg.norm(yhat, ord=1)
-    y = yhat + beta*v
-    residual = np.linalg.norm(y - z, ord=1)
-    i = i + 1
+# z = np.ones(len(column_sum)) / len(column_sum)
+# z = z.T
+# v = np.ones(len(column_sum)) / len(column_sum)
+# while residual > epsilon and i < 100:
+#     yhat = alpha * M * z
+#     beta = 1 - np.linalg.norm(yhat, ord=1)
+#     yhat = yhat + beta*v
+#     residual = np.linalg.norm(yhat - z, ord=1)
+#     i = i + 1
+# print(z)
 
+r = np.ones(len(column_sum)) / len(column_sum)
+rhat = np.zeros(len(column_sum))
+# print(r.shape)
+print(M.shape)
+print(rhat.shape)
 
-print(z)
+while residual > epsilon:
+    rhat = alpha * np.matmul(A, r)
+    rhat = rhat / np.linalg.norm(rhat, ord=1) 
+    residual = np.linalg.norm(rhat - r, ord=1)
+    r = rhat
+    print(residual)
+
+print(rhat)
+
+import matplotlib.pyplot as plt
+fig = plt.figure()
+ax = fig.add_axes([0, 0, 1, 1])
+x = np.arange(len(column_sum))
+y = rhat
+print(x.shape)
+print(y.shape)
+ax.stem(x,y)
+plt.show()
+
+m = max(rhat)
+b = np.where(rhat == m)
+print(b)
